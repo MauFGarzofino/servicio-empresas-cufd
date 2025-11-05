705583538d82ea65eab8623caf297b7fb7ce1cac
MauFGarzofino <mauricioflores123321@gmail.com>
2025-11-05 11:54:03 -0400

    script mongo-init.js

--- scripts/mongo-init.js
+++ scripts/mongo-init.js
@@ -1,20 +1,39 @@
 db = db.getSiblingDB('empresas');
 
-// colecciones base
+// Colecciones base
 db.createCollection('empresas');
 db.createCollection('sucursales');
 db.createCollection('puntos_venta');
 db.createCollection('cufd');
 
-// índices
+// Índices y unicidad
 db.empresas.createIndex({ nit: 1 }, { unique: true });
-db.sucursales.createIndex({ empresaId: 1, codigoSucursal: 1 }, { unique: true });
-db.puntos_venta.createIndex({ sucursalId: 1, codigoPuntoVenta: 1 }, { unique: true });
 
-// un CUFD activo por (sucursal, pdv)
+db.sucursales.createIndex(
+    { empresaId: 1, codigoSucursal: 1 },
+    { unique: true }
+);
+db.sucursales.createIndex({ empresaId: 1 });
+
+db.puntos_venta.createIndex(
+    { sucursalId: 1, codigoPuntoVenta: 1 },
+    { unique: true }
+);
+db.puntos_venta.createIndex({ sucursalId: 1 });
+
+// 1 CUFD activo por (Sucursal,PdV)
 db.cufd.createIndex(
     { codigoSucursal: 1, codigoPuntoVenta: 1 },
     { unique: true, partialFilterExpression: { estado: "ACTIVO" } }
 );
 
-print("Indices de servicio-empresas-cufd creados");
+// ejemplo
+db.empresas.insertOne({
+    nit: "1020304050",
+    razonSocial: "Acme SRL",
+    correo: "contacto@acme.bo",
+    estado: true,
+    creadoEn: new Date()
+});
+
+print("Colecciones e índices creados en 'empresas'.");
